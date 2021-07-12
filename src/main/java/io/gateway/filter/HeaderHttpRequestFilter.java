dcf4f4993a084eed5f766a4fd8be61a9c99b7e5f
zhu.diqing@21vianet.com <zhu.diqing@21vianet.com>
2021-07-12 15:31:25 +0800

    Modified the HTTP request filter.

--- src/main/java/io/gateway/filter/HeaderHttpRequestFilter.java
+++ src/main/java/io/gateway/filter/HeaderHttpRequestFilter.java
@@ -8,7 +8,7 @@ public class HeaderHttpRequestFilter implements HttpRequestFilter {
     @Override
     public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
         fullRequest.headers().set("IDENTITY", "JWT20210621");
-        fullRequest.headers().set("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTI4OTA4MDUsImlzcyI6Imh0dHA6Ly8yMXZjbG91ZC5jb20iLCJ1c2VyX2lkIjoiOGI5ZTkxMDE4ZDgyNDBmODgxNWMwMTRlYTllNWI5NzYiLCJjb21wYW55X29iaiI6IjVkZDc4ZDE0ZjRmM2E0ZjE2MDlkNTczOSJ9.QSQ3al8jricAxX_AyJPpo_gQCi1fJSLj8GUAP6skEJc");
+        fullRequest.headers().set("token", "ignore-fc3VwZXJ1c2VyIjp0cnVlLCJ0ZW5hbnQiOm51bGwsImV4cCI6MTYyMTU4NDM1OCwiaXNzIjo");
         fullRequest.headers().setInt("Content-Length", fullRequest.content().readableBytes());
         fullRequest.headers().set("Content-Type", "application/json");
     }
